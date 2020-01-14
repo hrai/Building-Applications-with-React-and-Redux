3198e8d51e33c715ab4a59e02467b417f10af284
Hangjit Rai <rai.hangjit@gmail.com>
2020-01-14 17:50:41 +1100

    Added Button And Handler

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+    constructor() {
+        super();
+
+        this.state={
+            course: {
+                title: ''
+            }
+        };
+    }
+
+    handleChange= (event)=>{
+        const course={...this.state.course, title:event.target.value};
+        this.setState({course});
+    }
+
     render() {
-        return <h2>Courses</h2>;
+        return (
+            <form>
+                <h2>Courses</h2>
+                <h3>Add course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="Save" />
+            </form>
+        );
     }
 }
 
