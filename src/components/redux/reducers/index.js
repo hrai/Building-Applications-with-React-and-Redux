f503b959bb114bb42a7735103eb29bef3ea3528d
Hangjit Rai <rai.hangjit@gmail.com>
2020-01-15 15:34:04 +1100

    Cleanup

--- src/components/redux/reducers/index.js
+++ src/components/redux/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from "redux";
-import courses from "./courseReducer";
+import { combineReducers } from 'redux';
+import courses from './courseReducer';
 
 const rootReducer=combineReducers({courses});
 
