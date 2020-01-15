f9dd348b29f6f90ab1f609da372f063fe1c76ff8
Hangjit Rai <rai.hangjit@gmail.com>
2020-01-15 16:49:41 +1100

    Cleanup

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
-export default function configureStore(initialState){
-    const composeEnhancers =
+export default function configureStore(initialState) {
+  const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
 
-  createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()) ));
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
 }
