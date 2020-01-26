721e71a9d2c9c3267174f3991b8e30aafbc06d2c
Hangjit Rai <rai.hangjit@gmail.com>
2020-01-26 14:19:02 +1100

    Added Const File

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -4,34 +4,14 @@ import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 
 class CoursesPage extends React.Component {
-    state={
-        course: {
-            title: ''
-        }
-    };
-
-    handleChange= (event)=>{
-        const course={...this.state.course, title:event.target.value};
-        this.setState({course});
-    }
-
-    handleSubmit=(event)=>{
-        this.props.courseActions.createCourse(this.state.course);
-
-        event.preventDefault();
-    }
-
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Courses</h2>
-                <h3>Add course</h3>
-                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
-                <input type="submit" value="Save" />
                 {this.props.courses.map(course=>(
                     <div key={course.title}>{course.title}</div>
                 ))}
-            </form>
+            </>
         );
     }
 }
