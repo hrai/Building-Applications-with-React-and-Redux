93623fd4cce7c27a4b81ea21d5493c58e753ca35
Hangjit Rai <rai.hangjit@gmail.com>
2020-01-29 17:10:38 +1100

    Updated Action

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,15 +1,19 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course};
-}
-
 export function loadCourseSuccess(courses){
     return {
         type: types.LOAD_COURSES_SUCCESS,
         courses
     };
+}
+
+export function createCourseSuccess(course){
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course){
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses(){
@@ -19,5 +23,21 @@ export function loadCourses(){
         }).catch(error=>{
             throw error;
         });
+    };
+}
+
+
+export function saveCourse(course) {
+    return function (dispatch, getState) {
+        return courseApi
+            .saveCourse(course)
+            .then(savedCourse => {
+                course.id
+                    ? dispatch(updateCourseSuccess(savedCourse))
+                    : dispatch(createCourseSuccess(savedCourse))
+            })
+            .catch(error => {
+                throw error;
+            });
     };
 }
