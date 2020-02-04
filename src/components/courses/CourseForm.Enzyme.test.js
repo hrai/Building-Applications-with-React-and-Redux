d65f5542fe92e5a48df4d96651f520a37d3c7934
Hangjit Rai <rai.hangjit@gmail.com>
2020-02-04 18:43:46 +1100

    Added Mount Tests

--- src/components/courses/CourseForm.Enzyme.test.js
+++ src/components/courses/CourseForm.Enzyme.test.js
@@ -24,3 +24,13 @@ it("renders form and header", () => {
   expect(wrapper.find('form').length).toBe(1);
   expect(wrapper.find('h2').text()).toEqual('Add Course');
 });
+
+it('labels save buttons as "Save" when not saving', () => {
+  const wrapper = renderCourseForm();
+  expect(wrapper.find("button").text()).toBe("Save");
+});
+
+it('labels save button as "Saving..." when saving', () => {
+  const wrapper = renderCourseForm({ saving: true });
+  expect(wrapper.find("button").text()).toBe("Saving...");
+});
