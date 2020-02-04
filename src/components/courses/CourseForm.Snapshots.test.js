d1e238bd1fb05374264a13cd6ce56431ef64cb95
Hangjit Rai <rai.hangjit@gmail.com>
2020-02-04 18:14:23 +1100

    Added Snapshots

--- src/components/courses/CourseForm.Snapshots.test.js
+++ src/components/courses/CourseForm.Snapshots.test.js
@@ -17,3 +17,17 @@ it("sets submit button label 'Saving...' when saving is true", () => {
 
   expect(tree).toMatchSnapshot();
 });
+
+it("sets submit button label 'Save...' when saving is false", () => {
+  const tree=renderer.create(
+    <CourseForm
+    course={courses[0]}
+    authors={authors}
+    onSave={jest.fn()}
+    onChange={jest.fn()}
+    saving={false}
+    />
+  );
+
+  expect(tree).toMatchSnapshot();
+});
