17c6f9265f6adff23a8f00d5f6cf72928feb26df
Hangjit Rai <rai.hangjit@gmail.com>
2020-02-04 18:10:16 +1100

    Updated Tests

--- 09/demos/after/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import courses from "./courseReducer";
-
-const rootReducer = combineReducers({
-  courses
-});
-
-export default rootReducer;
--- 09/demos/before/src/components/courses/CoursesPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class CoursesPage extends React.Component {
-  render() {
-    return <h2>Courses</h2>;
-  }
-}
-
-export default CoursesPage;
